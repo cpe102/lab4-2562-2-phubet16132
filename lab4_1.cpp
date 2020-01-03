bf3d7fb1cb1c29b07057c05e1652af0cf91b439d
phubet16132 <59276795+phubet16132@users.noreply.github.com>
2020-01-03 15:03:50 +0700

    compeled

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+#include<cmath>
 
+using namespace std ;
+
+const float e = 2.71828 ;
+
+float FineY(float x)
+	{
+		float y = (3*pow(x,3))+(2*e)+pow(2,2*x+1)+pow(pow(x,2)+1,0.5);
+		return y ;
+	}
+	
+int main()
+	{
+		int x;
+		
+		cout<<"Enter x = ";
+		cin>> x ;
+		cout<<"Result y = "<< FineY(x) ;
+		
+		return 0;
+	}
