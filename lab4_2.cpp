e2ab307e7964957de9d2afab47ea89ebfc116e76
phubet16132 <59276795+phubet16132@users.noreply.github.com>
2020-01-03 15:15:44 +0700

    compeled

--- lab4_2.cpp
+++ lab4_2.cpp
@@ -4,9 +4,22 @@ using namespace std;
 
 //Write function findDistance() here
 
-int main(){
+float findDistance(float u,float a,float t)
+	{
+		float s = (u*t)+(0.5*a*(t*t));
+		return s ;
+	}
+int main()
+	{
 
-  //Calling findDistance() using test cases
-
-  return 0;
-}
+  		//Calling findDistance() using test cases
+		
+		cout<<findDistance(0,0.5,1)<<endl;
+		
+		cout<<findDistance(1.5,-1,2)<<endl;
+	
+		cout<<findDistance(5,4,3)<<endl;
+		
+		
+  		return 0;
+	}
