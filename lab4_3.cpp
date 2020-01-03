9ed42cf367c66451729c4f78fadbfea3bcbe7264
phubet16132 <59276795+phubet16132@users.noreply.github.com>
2020-01-03 15:37:58 +0700

    compeled

--- lab4_3.cpp
+++ lab4_3.cpp
@@ -1,1 +1,47 @@
+#include<iostream>
 
+
+using namespace std ;
+
+
+float findDivisor(int num1)
+	{
+		int i=2;
+		
+		
+		if(num1 > 1)
+		{
+			while(i > 1 && i < num1)
+			{
+				if(num1 % i !=0)
+				{
+					i=i+1;
+				} 
+				else
+				{
+					return i ;
+				}		
+			}
+			
+		}
+		else 
+		{
+			return -1 ;
+		}
+		
+		
+	}
+
+
+
+int main()
+{
+	int num1 ;
+	
+	cout<<"Enter num1 = ";
+	cin>>num1;
+	cout<<findDivisor(num1);
+	
+	
+	return 0 ;
+}	
